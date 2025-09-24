dca88e167d6d1564c8ea01d0218456820e29425e
Bronny721 <bear20030721@gmail.com>
2025-09-24 15:11:40 +0800

    新增統計分析頁面路由

--- traffic-system/src/App.tsx
+++ traffic-system/src/App.tsx
@@ -5,7 +5,7 @@ import Dashboard from './pages/Dashboard';
 // 為了讓路由完整，我們先建立幾個空的範例頁面
 import ViolationLog from './pages/ViolationLog';
 import GenerateTickets from './components/violations/GenerateTickets';
-//import Analytics from './pages/Analytics';
+import Analytics from './pages/Analytics';
 //import SystemManagement from './pages/SystemManagement';
 
 
@@ -16,7 +16,7 @@ function App() {
         <Route index element={<Dashboard />} />
         <Route path="violations" element={<ViolationLog />} />
         <Route path="/generate-tickets" element={<GenerateTickets />} />
-        {/*<Route path="analytics" element={<Analytics />} /> 
+        <Route path="analytics" element={<Analytics />} /> 
         {/*<Route path="system" element={<SystemManagement />} />
         {/* 你可以在這裡加入其他頁面的路由 */}
       </Route>
